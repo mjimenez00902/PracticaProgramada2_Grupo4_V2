0b7b133efd37cbbbee22b0577507b457c4974e67
mjimenez00902 <mjimenez00902@ufide.ac.cr>
2026-06-22 21:24:20 -0600

    Update Producto.java

--- src/main/java/com/fidelitas/java/practicaprogramada2/Producto.java
+++ src/main/java/com/fidelitas/java/practicaprogramada2/Producto.java
@@ -8,6 +8,15 @@ package com.fidelitas.java.practicaprogramada2;
  *
  * @author marcojimenezmena
  */
-public class Producto {
-    
+public class Producto extends ProductoBase {
+
+    //Constructor
+    public Producto(String codigo, String nombre, double precio, int cantidadInventario) {
+        super(codigo, nombre, precio, cantidadInventario);
+    }
+
+    @Override
+    public double obtenerPrecioFinal() {
+        return getPrecio(); 
+    }
 }
