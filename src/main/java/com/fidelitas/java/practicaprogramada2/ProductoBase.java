46a70bb698620712f8b550a5393d805f7a04ac97
mjimenez00902 <mjimenez00902@ufide.ac.cr>
2026-06-22 21:23:41 -0600

    Update ProductoBase.java

--- src/main/java/com/fidelitas/java/practicaprogramada2/ProductoBase.java
+++ src/main/java/com/fidelitas/java/practicaprogramada2/ProductoBase.java
@@ -8,6 +8,56 @@ package com.fidelitas.java.practicaprogramada2;
  *
  * @author marcojimenezmena
  */
-public class ProductoBase {
+public abstract class ProductoBase {
+
+    private String codigo;
+    private String nombre;
+    private double precio;
+    private int cantidadInventario;
+
+    //Constructor
+    public ProductoBase(String codigo, String nombre, double precio, int cantidadInventario) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidadInventario = cantidadInventario;
+    }
+
+    // --- Método Abstractos ---
     
+    public abstract double obtenerPrecioFinal();
+
+    // --- Getters y Setters ---
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getCantidadInventario() {
+        return cantidadInventario;
+    }
+
+    public void setCantidadInventario(int cantidadInventario) {
+        this.cantidadInventario = cantidadInventario;
+    }
 }
