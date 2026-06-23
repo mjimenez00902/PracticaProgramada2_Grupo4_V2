8c48a17a19aa00dbb128f313af867b3505dfc2d5
mjimenez00902 <mjimenez00902@ufide.ac.cr>
2026-06-22 21:25:07 -0600

    Update ProductoDescuento.java

--- src/main/java/com/fidelitas/java/practicaprogramada2/ProductoDescuento.java
+++ src/main/java/com/fidelitas/java/practicaprogramada2/ProductoDescuento.java
@@ -8,6 +8,30 @@ package com.fidelitas.java.practicaprogramada2;
  *
  * @author marcojimenezmena
  */
-public class ProductoDescuento {
+public class ProductoDescuento extends ProductoBase {
+
+    private double porcentajeDescuento;
+
+    //Constructor
     
+    public ProductoDescuento(String codigo, String nombre, double precio, 
+                             int cantidadInventario, double porcentajeDescuento) {
+        super(codigo, nombre, precio, cantidadInventario);
+        this.porcentajeDescuento = porcentajeDescuento;
+    }
+    
+    @Override
+    public double obtenerPrecioFinal() {
+        return getPrecio() - (getPrecio() * (porcentajeDescuento / 100));
+    }
+
+    // --- Getters y Setters ---
+
+    public double getPorcentajeDescuento() {
+        return porcentajeDescuento;
+    }
+
+    public void setPorcentajeDescuento(double porcentajeDescuento) {
+        this.porcentajeDescuento = porcentajeDescuento;
+    }
 }
